a07de9a8a05b22c6a352611be3f7e5da21d06211
MMorlaye <smorlaye313@gmail.com>
2025-05-27 10:45:03 +0200

    Modification du fichier next.config.js et de vercel.json pour le déploiement

--- morlaye-portfolio/next.config.js
+++ morlaye-portfolio/next.config.js
@@ -1,7 +1,22 @@
+/** @type {import('next').NextConfig}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Si vous utilisez des liens internes
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
 module.exports = nextConfig
